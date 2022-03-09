fdd1f2f29357f33a342dee5b9b11c2b987bca105
bb2100 <albin.ekelunden@gmail.com>
2022-03-09 14:16:21 +0100

    Made the program work

--- main.py
+++ main.py
@@ -6,12 +6,25 @@ class Planet:
 
     def get_name(self):
         return self.name
+
     def get_size(self):
         return self.size
+
     def get_moons(self):
         return self.moons
+        
     def add_moon(self, moon):
-        self.moon = moon
+        self.moons.append(moon)
+
+    def print_moons(self):
+        for moon in self.moons:
+            print(moon.get_name())
+
+    def certain_size(self, size : int):
+        print(f"The following moons around {self.get_name()} larger than {size} sqkm:")
+        for moon in self.moons:
+            if moon.get_size() > size:
+                print(moon.get_name())
 
 class Moon:
     def __init__(self, name : str, size : int):
@@ -37,6 +50,18 @@ def main():
     europa = Moon("Europa", 19613)
     ganymedes = Moon("Ganymedes", 33101)
 
+    tellus.add_moon(luna)
+    mars.add_moon(ganymedes)
+    jupiter.add_moon(europa)
+
+    luna.add_orbit(tellus)
+    ganymedes.add_orbit(mars)
+    europa.add_orbit(jupiter)
+
+    tellus.print_moons()
+    jupiter.print_moons()
+
+    mars.certain_size(1000)
 
 if __name__ == "__main__":
     main()
